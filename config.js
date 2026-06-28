8e9822f711c12efd4f0c3529849e0fe7d594f2b8
kasama <4schooldoc@gmail.com>
2026-06-28 21:01:18 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Supabase Configuration
-const SUPABASE_URL = 'YOUR_SUPABASE_URL';
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
+const SUPABASE_URL = 'https://prsgpaqbokothjsqclyv.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InByc2dwYXFib2tvdGhqc3FjbHl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2NjQ2NzIsImV4cCI6MjA5ODI0MDY3Mn0.9aQAYye-50gwbK0V84RS8p_nS75bfr9fkG27c6QPODY';
 
 // Initialize Supabase client
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
